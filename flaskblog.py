fdd5f6683e585e12a070705350519db6a07f9239
Youness <elkhatabi.youness@gmail.com>
2020-11-14 13:49:56 +0100

    add about route

--- flaskblog.py
+++ flaskblog.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Home Page</h1>'
 
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
