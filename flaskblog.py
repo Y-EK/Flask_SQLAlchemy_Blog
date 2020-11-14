9e26304ec63a63faec05190f053d4aeaa5192254
Youness <elkhatabi.youness@gmail.com>
2020-11-14 14:18:21 +0100

    add posts list

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 26, 2020'
+    },
+    {
+        'author': 'James Gonzales',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 14, 2020'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
