c20fc78a4dff2c93a955f382f50c0db8ba259292
Youness <elkhatabi.youness@gmail.com>
2020-11-15 15:34:49 +0100

    introduce validation example of login

--- flaskblog.py
+++ flaskblog.py
@@ -38,9 +38,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form) 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please check username and password!', 'danger')    
     return render_template('login.html', title='Login', form=form)        
 
 if __name__ == '__main__':
